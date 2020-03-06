d2cdc323452c913320c9fba9c9e2f98dac502dad
Ryan-Oroke <49414263+Ryan-Oroke@users.noreply.github.com>
2020-03-05 19:47:14 -0700

    Fixed the function file for upload page

---
+++ static/upload.js
@@ -0,0 +1,4 @@
+function distanceSlid(){
+    var dist = document.getElementById("visibleDistanceSlider").value;
+    document.getElementById("visibleDistanceLabel").innerHTML = "<h5>" + dist + " Feet </h5>";
+}
