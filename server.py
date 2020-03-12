f7d23a5bfa969d37c8e3f581b1aa0168c419368f
ZackJorquera <jorquerazack@gmail.com>
2020-03-11 20:15:51 -0600

    added tester code

--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 # and the post get stuff
 
 import os
+from typing import NamedTuple
 
 from flask import Flask, render_template, jsonify, abort, request, make_response, url_for
 
@@ -14,14 +15,20 @@ app.secret_key = "Not Random. Oh Noes!"  # This is for metadata encryption
 
 # CORS(app)
 
+
+class file_data_html(NamedTuple):
+    name: str
+    lat: float
+    long: float
+    format: str
+    path: str
+    dist: float
+    id: int
+
+
 @app.route('/')
 def root_route():
     return render_template("master.html")
-
-
-@app.route('/login')
-def login_page():
-    return render_template("login.html")
 
 
 @app.route('/upload')
@@ -31,7 +38,10 @@ def upload_page():
 
 @app.route('/download')
 def download_page():
-    return render_template("download.html")
+    test_data = [file_data_html("test1", 40.015869, -105.279517, "dick", "123", 100, 12), file_data_html("test2", 40.016869, -105.279617, "dick", "123", 101, 34)]
+
+
+    return render_template("download.html", fils=test_data)
 
 
 @app.route('/navbar')
