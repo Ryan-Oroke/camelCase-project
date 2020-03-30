fb565de56b2bff7708df43d513905ed8ade1ebdf
Ryan-Oroke <49414263+Ryan-Oroke@users.noreply.github.com>
2020-03-30 15:22:02 -0600

    minor stuff

--- static/functions.js
+++ static/functions.js
@@ -15,21 +15,6 @@ function getLocation(initMap)
   }
 }
 
-// function initMap()
-// {
-//     //Add a higher order function/"promises"
-//     // The location of Uluru
-//     //getLocation();
-//     //var uluru = { lat: latVar, lng: lonVar };
-//     var uluru = { lat: 40.050367699999995, lng: -105.24975579999999 };
-//     // The map, centered at Uluru
-//     //alert(latVar);
-//     var map = new google.maps.Map(
-//         document.getElementById('map'), { zoom: 14, center: uluru });
-//     // The marker, positioned at Uluru
-//     var marker = new google.maps.Marker({ position: uluru, map: map });
-// }
-
 //For more information or much of this code, see: https://developers.google.com/maps/documentation/javascript/geolocation
 var map, infoWindow;
 
