34a4068338d7d40fc562f6e022c3a1d1a58f5c0f
ZackJorquera <jorquerazack@gmail.com>
2020-04-10 09:35:16 -0600

    made more backend changed needed for UI update

--- server.py
+++ server.py
@@ -2,13 +2,15 @@ import os
 import hashlib
 from typing import NamedTuple
 
-from flask import Flask, render_template, jsonify, abort, request, make_response, url_for, session, send_from_directory
+from flask import Flask, render_template, jsonify, abort, request, make_response, url_for, session, send_from_directory, send_file
 
 app = Flask(__name__)
 app.secret_key = "Not Random. Oh Noes!"  # This is for metadata encryption (using session)
 
-UPLOAD_DIRECTORY = 'static/upload_files'  # this is in static so we dont have to write any code to expose files for
-                                          # preview. Node: this has a security down side as every file can be accessed.
+UPLOAD_DIRECTORY = 'upload_files'  # this is in static so we dont have to write any code to expose files for
+                                   # preview. Note: this has a security down side as every file can be accessed.
+# The static is implied. You must use url_for('static', filename='...') or an os.path.join('static', ...)
+# or would it make sense to have a folder for preview images in static and the full files in UPLOAD_DIRECTORY.
 
 
 @app.errorhandler(400)
@@ -30,7 +32,7 @@ class file_data_html(NamedTuple):
     lat: float
     long: float
     format: str
-    path: str
+    path: str  # a path starting in static/UPLOAD_DIRECTORY
     dist: float
     id: int
     description: str
@@ -53,9 +55,12 @@ def get_signed_in_info():
 
 
 def get_downloadable_files():
+    # I thing mongo will store "test_user/P1540913.JPG" for the path and then we need to add the UPLOAD_DIRECTORY part
+    # or we could only store "P1540913.JPG" and add UPLOAD_DIRECTORY and user name
 
-    return [file_data_html("test1", 40.015869, -105.279517, "jpg", "test_user/P1540913.JPG", 100, 12, "abc", "today", "tomorrow", False, "", 0, "test_user"),
-            file_data_html("test2", 40.016869, -105.279617, "jpg", "test_user/P1540506.JPG", 101, 34, "def", "tomorrow", "Mar 3", True, hashlib.sha256("password1".encode('utf-8')).hexdigest(), 1, "test_user")]
+    return [file_data_html("test1", 40.015869, -105.279517, "jpg", os.path.join(UPLOAD_DIRECTORY, "test_user/P1540913.JPG"), 100, 12, "abc", "today", "tomorrow", False, "", 0, "test_user"),
+            file_data_html("test2", 40.016869, -105.279617, "jpg", os.path.join(UPLOAD_DIRECTORY, "test_user/P1540506.JPG"), 101, 34, "def", "tomorrow", "Mar 3", True, hashlib.sha256("password".encode('utf-8')).hexdigest(), 1, "test_user")]
+    # Note, we want to store the path starting in static/ but not including the static part
 
 
 def handle_login_post():
@@ -133,8 +138,9 @@ def handle_upload_post(signed_in, cur_user, file_data):
         if not os.path.exists(os.path.join(UPLOAD_DIRECTORY, cur_user)):
             os.makedirs(os.path.join(UPLOAD_DIRECTORY, cur_user))
         filename = os.path.join(cur_user, input_file.filename)
-        input_file.save(os.path.join(UPLOAD_DIRECTORY, filename))  # maybe check if file exists too
-        # TODO: add data to mongo
+        file_path = os.path.join('static', UPLOAD_DIRECTORY, filename)
+        input_file.save(file_path)  # maybe check if file exists too
+        # TODO: add data to mongo (Note we will store filename)
 
     return render_template("download.html", fils=file_data, signed_in=signed_in, cur_user=cur_user, failed_password=False)
 
@@ -150,8 +156,8 @@ def handle_download_post(signed_in, cur_user, this_file_data, file_data):
         if input_password_hash != password_hash:
             return render_template("download.html", fils=file_data, signed_in=signed_in, cur_user=cur_user,
                                    failed_password=True)
-
-    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+    # print(os.path.join('static', path))
+    return send_file(os.path.join('static', path), as_attachment=True)
 
 
 @app.route('/download', methods=['GET'])
@@ -181,6 +187,7 @@ def download_page_post():
     else:
         for this_file_data in file_data:
             if str(this_file_data.id) in request.form:
+                print('hit')
                 return handle_download_post(signed_in, cur_user, this_file_data, file_data)
 
     abort(400)
